f1b700d0e938ea8322e1bbd48a4ee7a519270421
Suppaakij Boonravee <colorgray.love@hotmail.com>
2015-08-29 14:38:58 +0700

    Bind Widget Finish

---
+++ app/src/main/java/com/example/jk/myapplication/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.jk.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //ประกาศตัวแปล
+
+    private ListView trafficListView;
+    private Button aboutMeButtom;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+
+
+
+    } // เมธอทหลัก
+
+    private void bindWidget() {
+
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButtom = (Button) findViewById(R.id.button);
+
+
+    } //Bind Widget
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+} //Main Class
