34bdd4946d6f81fe4ee771a4795ff924cea7116a
Suppaakij Boonravee <colorgray.love@hotmail.com>
2015-08-30 10:36:23 +0700

    Traddic day 2

--- app/src/main/java/com/example/jk/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jk/myapplication/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.jk.myapplication;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -23,10 +27,33 @@ public class MainActivity extends AppCompatActivity {
         //Bind Widget
         bindWidget();
 
+        //ButtonController
+        buttonController();
+
 
 
 
     } // เมธอทหลัก
+
+    private void buttonController() {
+
+        aboutMeButtom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.clickeffect1);
+                buttonMediaPlayer.start();
+
+                //intent to Webview
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("http://www.trafficpolice.go.th/"));
+                startActivity(objIntent);
+
+            }
+        });
+
+    } //button controller
 
     private void bindWidget() {
 
